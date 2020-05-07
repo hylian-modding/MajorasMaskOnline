d46a2cb636cbb67311760a86b526c848f3ccdd8e
Nick Sanguinetti <DJInsomniaPro@gmail.com>
2020-05-07 06:06:43 -0500

    Puppet Overhaul

---
+++ src/MMARO/c/structs.h
@@ -0,0 +1,23 @@
+typedef struct
+{
+	uint32_t base;	 // Base RAM Address
+	uint32_t skeleton;
+	uint16_t radius; // Collision Cylinder Radius
+	uint16_t height; // Collision Cylinder Height
+	float 	 rise;   // Units to be floated
+} puppet_init_t;
+
+typedef struct
+{
+    uint32_t Back;  // Item on Back
+    uint32_t Left;  // Held Item (Left Hand)
+    uint32_t Right; // Held Item (Right Hand)
+} puppet_items_t;
+
+typedef struct
+{
+	uint8_t isZZ;
+	uint16_t eye_index;
+	uint32_t eye_texture;
+	puppet_init_t init;
+} zzplayas_t;
